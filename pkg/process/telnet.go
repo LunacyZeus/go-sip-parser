fb72f4dac3d6163de765d7eaf6019ba0cbe4d7aa
zeusho <zeus@smallho.cn>
2024-11-21 06:24:25 +0800

    feat: 更新

--- pkg/process/telnet.go
+++ pkg/process/telnet.go
@@ -1,34 +1,10 @@
 package process
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"net"
 	"os"
-	"strings"
-	"time"
+	"sip-parser/pkg/telnet"
 )
-
-func sendLoginCommand(conn net.Conn, reader *bufio.Reader) (bool, error) {
-	command := "login\r\n"
-	// 发送命令
-	fmt.Printf("Sending: %s", command)
-	_, err := conn.Write([]byte(command))
-	if err != nil {
-		return false, fmt.Errorf("failed to send command: %w", err)
-	}
-
-	// 读取服务器响应
-	fmt.Println("Waiting for Login response...")
-	conn.SetReadDeadline(time.Now().Add(8 * time.Second)) // 设置超时时间
-	response, err := reader.ReadString('\n')
-	if err != nil {
-		return false, fmt.Errorf("failed to read response: %w", err)
-	}
-	fmt.Printf("login recv: %s", strings.TrimSpace(response))
-	return true, nil
-}
 
 // 将字符串写入文件
 func writeToFile(filename, content string) error {
@@ -48,95 +24,25 @@ func writeToFile(filename, content string) error {
 }
 
 func StartTelnet(csvFilePath string) {
-	log.Println(csvFilePath)
-
-	// 设置连接超时时间（例如 10 秒）
-	dialer := &net.Dialer{
-		Timeout: 25 * time.Second, // 设置连接超时
+	tc := telnet.TelnetClient{
+		Address:  "127.0.0.1",
+		Port:     "4320",
+		Login:    "",
+		Password: "",
 	}
-
-	// 连接到 127.0.0.1:4320
-	conn, err := dialer.Dial("tcp", "127.0.0.1:4320")
+	err := tc.Dial()
 	if err != nil {
-		fmt.Println("Error connecting to server:", err)
-		os.Exit(1)
+		fmt.Printf("failed open connect with error = %v\n", err)
+		return
 	}
+	defer tc.Close()
 
-	defer conn.Close()
-
-	err = conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+	stdout, err := tc.Execute("login")
 	if err != nil {
+		fmt.Printf("failed execute command with error = %v\n", err)
 		return
 	}
 
-	fmt.Println("Connected to 127.0.0.1:4320")
+	fmt.Printf(string(stdout))
 
-	// 创建读取器，用于接收服务器返回的数据
-	reader := bufio.NewReader(conn)
-
-	// 发送 login 命令
-	_, err = sendLoginCommand(conn, reader)
-	if err != nil {
-		fmt.Println("Error during login:", err)
-		os.Exit(1)
-	}
-
-	// 等待服务器响应后发送 call_simulation
-	call_simulation_resp, err := sendCommand(conn, "call_simulation 88.151.132.30,5060,9123887982,5482#+14049179360\r\n", reader)
-	if err != nil {
-		fmt.Println("Error during call_simulation:", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("call recv: %s", call_simulation_resp)
-	writeToFile("call", call_simulation_resp)
-
-}
-func sendCommand(conn net.Conn, command string, reader *bufio.Reader) (string, error) {
-	// 发送命令
-	fmt.Printf("Sending command: %s", command)
-	_, err := conn.Write([]byte(command))
-	if err != nil {
-		return "", fmt.Errorf("failed to send command: %w", err)
-	}
-
-	// 设置超时时间（5秒）
-	timeoutDuration := 5 * time.Second
-	timer := time.NewTimer(timeoutDuration)
-	defer timer.Stop()
-
-	// 读取多行服务器响应，拼接成一个字符串
-	var responseBuilder strings.Builder
-	for {
-		select {
-		case <-timer.C: // 超时
-			log.Println("i/o timeout")
-			return "", fmt.Errorf("read timeout after %v", timeoutDuration)
-		default:
-			// 从连接读取数据
-			line, err := reader.ReadString('\n')
-			if err != nil {
-				// 检查错误是否是超时错误
-				if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-					log.Println("i/o timeout")
-					return "", fmt.Errorf("failed to read response: %w", err)
-				}
-				// 其他错误
-				return "", fmt.Errorf("failed to read response: %w", err)
-			}
-
-			// 将读取的行追加到响应结果
-			responseBuilder.WriteString(line)
-
-			// 如果返回某些预定义的结束标记，可以在此处判断并终止读取
-			if strings.HasSuffix(line, "<Call Simulation Test progress>Done</Call Simulation Test progress>") {
-				break
-			}
-
-			// 重置超时定时器
-			timer.Reset(timeoutDuration)
-		}
-	}
-
-	return responseBuilder.String(), nil
 }
